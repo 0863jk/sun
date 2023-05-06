30ae012331f0a13abb6c4ff6aec877cf59d0d3b6
0863jk <0863jk@naver.com>
2023-05-07 00:28:39 +0900

    first commit

--- venv/sun_back/accounts/models.py
+++ venv/sun_back/accounts/models.py
@@ -6,8 +6,7 @@ class User(AbstractUser):
     ROLE_CHOICES = (('manager', '매니저'),
                     ('trainer', '트레이너'),
                     ('general', '일반사용자'))
-    username = models.CharField(max_length=150, unique=False, verbose_name='username')
-    # email = models.EmailField(unique=True, verbose_name=("Email Address"))
-    phone = models.CharField(null=False, blank=False, unique=False, max_length=255)
     # 기본적으로 제공하는 필드 외에 원하는 필드를 적어준다.
+    username = models.CharField(max_length=150, unique=False, verbose_name='username')
+    phone = models.CharField(null=False, blank=False, unique=False, max_length=255)
     role = models.CharField(choices=ROLE_CHOICES, null = True, blank = True, max_length=255)
