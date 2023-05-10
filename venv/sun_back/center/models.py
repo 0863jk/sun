a6b9ffe8e42eb5d80e50923ba751daa4e834e9a0
0863jk <0863jk@naver.com>
2023-05-11 00:58:42 +0900

    DB 구조 수정

--- venv/sun_back/center/models.py
+++ venv/sun_back/center/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Center(models.Model):
     centername = models.CharField(max_length=200)
-    centerid = models.CharField(max_length=200)
+    centerid = models.CharField(max_length=200, unique=True)
     introduction = models.CharField(max_length=200)
     manager = models.CharField(max_length=200, null = True, blank = True)
     # manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,26 +13,35 @@ class Center(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.centername
+        return self.centerid
     
 class CenterPlan(models.Model):
     SYSTEM_CHOICE = (('fixed-term', '기간제'),
                     ('number-of-times', '횟수제'))
+    planid = models.AutoField(primary_key=True)
     planname = models.CharField(max_length=200)
     centerid = models.CharField(max_length=200)
-    period = models.CharField(max_length=200)
     plantype = models.CharField(choices=SYSTEM_CHOICE, null = False, max_length=255)
+    period = models.CharField(max_length=200)
     constraints = models.CharField(max_length=200)
+    price = models.IntegerField()
 
     def __str__(self):
-        return self.centername
+        return self.planid
     
-# class CenterMember(models.Model):
-#     centername = models.CharField(max_length=200)
-#     introduction = models.CharField(max_length=200)
-#     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     location = models.CharField(max_length=200)
-#     created_date = models.DateTimeField(default=timezone.now)
+class CenterTrainer(models.Model):
+    centerid = models.CharField(max_length=200)
+    userid = models.CharField(max_length=200, null = True, blank = True)
 
-#     def __str__(self):
-#         return self.centername
+    def __str__(self):
+        return self.id
+    
+class CenterMember(models.Model):
+    centerid = models.CharField(max_length=200)
+    userid = models.CharField(max_length=200, null = True, blank = True)
+    planid = models.IntegerField()
+    # registerDate = models.DateTimeField
+
+    def __str__(self):
+        return self.id
+    
