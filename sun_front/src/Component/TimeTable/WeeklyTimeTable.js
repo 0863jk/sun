6e27fd70fd012b4a6292116f4f29e80fee759ea3
0863jk <0863jk@naver.com>
2023-05-23 19:16:46 +0900

    ✨FEAT : 시간표 구현

---
+++ sun_front/src/Component/TimeTable/WeeklyTimeTable.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Link } from 'react-router-dom';
+
+const localizer = momentLocalizer(moment);
+
+const timetableData = [
+    {
+        title: 'Lesson A',
+        start: new Date(2023, 4, 22, 9, 0), // 예시 시간표 시작 시간
+        end: new Date(2023, 4, 22, 10, 0), // 예시 시간표 종료 시간
+        trainer: "trainer",
+        lessonId: 'lesson-id-1' // 예시 수업 아이디
+    }
+];
+
+function WeeklyTimetable() {
+    const events = timetableData.map((event) => {
+        return {
+            ...event,
+            start: moment(event.start).toDate(),
+            end: moment(event.end).toDate(),
+            title: `${event.title} (${event.trainer})`, // 강사 이름 추가
+        }
+    })
+    const handleEventClick = (event) => {
+        const lessonId = event.lessonId;
+        // 수업 상세 정보 페이지로 이동
+        // 예시: /lesson/lesson-id-1
+        window.location.href = `/lesson/${lessonId}`;
+    };
+
+    // const eventComponents = timetableData.map((event) => {
+    //     return {
+    //         ...event,
+    //         component: (
+    //             <Link to={`/lesson/${event.lessonId}`} className="calendar-event-link">
+    //                 {event.title}
+    //             </Link>
+    //         )
+    //     };
+    // });
+
+    const minTime = new Date();
+    minTime.setHours(9, 0, 0); // 8시
+
+    const maxTime = new Date();
+    maxTime.setHours(18, 0, 0); // 11시
+
+    return (
+        <div style={{ height: '600px' }}>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                views={['week']}
+                defaultView="week"
+                step={60} // 시간 단위 (60분)
+                defaultDate={new Date()}
+                min={minTime}
+                max={maxTime}
+                onSelectEvent={handleEventClick}
+                showMultiDayTimes={false}
+            />
+        </div>
+    );
+}
+
+export default WeeklyTimetable;
